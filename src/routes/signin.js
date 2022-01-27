0fd2472df2831ffe1376393aa81514f23a247c57
HMtsumura <hironori0811@gmail.com>
2022-01-27 23:40:36 +0900

    created logout router

--- src/routes/signin.js
+++ src/routes/signin.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models')
+const db = require('../models');
 
-router.get('/', function(req, res, next){
-    res.render('signin',{title: 'Sign in'});
+router.get('/', function (req, res, next) {
+    res.render('signin', { title: 'Sign in' });
 });
 
-router.post('/', async function(req, res, next){
+router.post('/', async function (req, res, next) {
     const userName = req.body.userName;
     const password = req.body.password;
 
-    const user = await db.User.findAll({where: {
-        name: userName,
-        password: password
-    }});
-    if(user.length !== 0){
+    const user = await db.User.findAll({
+        where: {
+            name: userName,
+            password: password
+        }
+    });
+    if (user.length !== 0) {
         req.session.userid = user[0].id;
         res.redirect('/');
-    }else{
+    } else {
         res.render("signin", {
-            title:  'Sign in',
+            title: 'Sign in',
             errorMessage: 'ユーザーが見つかりません'
         });
     }
