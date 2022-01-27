6b6573ebb2c21ab92552f5c4096a015b5d595af3
HMtsumura <hironori0811@gmail.com>
2022-01-27 21:59:27 +0900

    edited signin router, installed cookie-session

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const signupRouter = require('./routes/signup');
+const signinRouter = require('./routes/signin');
 
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/signin', signinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ src/routes/signin.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models')
+
+router.get('/', function(req, res, next){
+    res.render('signin',{title: 'Sign in'});
+});
+
+router.post('/', function(req, res, next){
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    const user = await db.User.findAll({where: {
+        name: userName,
+        password: password
+    }});
+    if(user.length !== 0){
+        res.render('/');
+    }else{
+        res.render("signin", {
+            title:  'Sign in',
+            errorMessage: 'ユーザーが見つかりません'
+        });
+    }
+});
+
+module.exports = router;
