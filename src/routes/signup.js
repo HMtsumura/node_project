c11dd47c7ef06d6f3dd85ca472560f0b7fa029c5
HMtsumura <hironori0811@gmail.com>
2022-01-27 17:11:49 +0900

    added signup function

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.get('/', function(req, res, next){
     res.render('signup',{title: 'Sign up'});
 });
 
-router.post('/', function(req, res, next){
+router.post('/', async function(req, res, next){
     const userName = req.body.userName;
     const password = req.body.password;
     const repassword = req.body.repassword;
+
+    const user = await db.User.findAll({where: {
+        name: userName
+    }});
+    console.log(user);
+  if(user.length !== 0){
+    res.render("signup", {
+        title: "Sign up",
+        errorMessage: "このユーザ名は既に使われています",
+    });
+  }else if(password === repassword){
+    const newUser = db.User.build({
+        name: userName,
+        password: password
+      });
+      await newUser.save();
+      res.redirect('/');
+  } else {
+    res.render("signup", {
+      title: "Sign up",
+      errorMessage: "パスワードが一致しません",
+    });
+  }
 });
 module.exports = router;
